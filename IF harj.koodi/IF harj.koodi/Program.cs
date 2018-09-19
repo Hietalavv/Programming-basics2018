41fd32c291d5b5da5bea0dfb976d01875ae8c03b
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-09-19 12:37:12 +0300

    IF harj.koodi initialized

---
+++ IF harj.koodi/IF harj.koodi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace iftask1
+{
+    //this on line comment 
+    class Program
+    {
+        
+        static void Main(string[] args)
+            
+        {
+        
+         Console.WriteLine("Program solves if the given number is positive, negative or zero.");
+            Console.WriteLine("")
+        }
+    }
+}
