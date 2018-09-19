f3ec622768e748aa3c67afd4b2f41cd84495f869
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-09-19 13:00:22 +0300

    IF harj.koodi valmis

--- IF harj.koodi/IF harj.koodi/Program.cs
+++ IF harj.koodi/IF harj.koodi/Program.cs
@@ -11,7 +11,18 @@ namespace iftask1
         {
         
          Console.WriteLine("Program solves if the given number is positive, negative or zero.");
-            Console.WriteLine("")
+            Console.WriteLine("Syötä numero");
+         string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla!");
+            else if (number < 0)
+                Console.WriteLine($"Numero {number} on negatiivinen");
+            else
+                Console.WriteLine($"Numero {number} on positiivinen");
+
+                    
         }
     }
 }
