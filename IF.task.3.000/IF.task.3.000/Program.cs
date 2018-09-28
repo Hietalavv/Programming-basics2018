26290e27c7dd05553124f2e2e75fe4f30c8acc5b
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-09-28 15:39:54 +0300

    IF.task.3 valmis

---
+++ IF.task.3.000/IF.task.3.000/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IF.task._3._000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko syötetty luku negatiivinen, positiivinen, nolla, parillinen tai pariton");
+            Console.WriteLine("syötä numero");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+            if (isNumber)
+            {
+                if (number == 0)
+                    Console.WriteLine($"Numero {number} on nolla");
+                else if (number < 0)
+                    Console.WriteLine($"Numero {number} on negatiivinen");
+                else
+                    Console.WriteLine($"Numero {number} on positiivinen");
+                if (number % 2 == 0)
+                    Console.WriteLine($"Ja {number} on parillinen");
+                else
+                    Console.WriteLine($"Ja {number} on pariton");
+
+            }
+            else
+                Console.WriteLine("Väärä syöte!");
+
+
+
+
+        }    
+
+    
+
+
+
+    }
+
+
+
+
+
+}
