d82ede1ade9ed4bc4baaca3a9ac5bab40145bf5e
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-10-10 09:26:50 +0300

    LoopTask1 valmis

---
+++ LoopTask1.0/LoopTask1.0/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LoopTask1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo annetun luvun summan");
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+                Console.Write("syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0) 
+                {
+                    Console.WriteLine("Virheellinen syöte!");
+                }
+            } while (number <= 0);
+
+            while (i <=number)
+            {
+                fact = fact * i; //i++;
+                i = i + 1; //i++;
+               
+            }
+            Console.WriteLine($"Luvun {number}! = {fact}");
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
