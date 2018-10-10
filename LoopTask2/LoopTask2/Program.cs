209513d55e1480c2420a94cc2b1ccd18f5ac0748
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-10-10 09:49:28 +0300

    LoopTask2 valmis

---
+++ LoopTask2/LoopTask2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoopTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee annetun luvun perusluvut yhteen");
+            int number = 0;
+            int i = 1;
+            int sum = 1;
+            do
+            {
+                Console.Write("syötä Luku; ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("virheellinen syöte!");
+                }
+            } while (number <= 0);
+            
+            while (i <=number)
+            {
+                sum = sum + i; 
+                i = i + 1; 
+            }
+            Console.WriteLine($"Luvun {number}! = {sum}");
+
+        }
+    }
+}
