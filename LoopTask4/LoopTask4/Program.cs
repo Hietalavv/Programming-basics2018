e0d5efc0e1d3aaca1258cde54279ff48d50178a6
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-10-11 12:59:59 +0300

    LoopTask4 valmis

---
+++ LoopTask4/LoopTask4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LoopTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee annetun luvun perusluvut yhteen. Myös negatiiviset");
+            int number = 0;
+            int i = 0;
+            int sum = 0;
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number == 0)
+                {
+                    Console.WriteLine("virheellinen syöte");
+                }
+            } while (number == 0);
+            if (number > 0)
+                {
+                while (i <=number)
+                {
+                    sum = sum + i;
+                    i = i + 1;
+
+                
+                }
+                
+            }
+            else
+            {
+                while (i >=number)
+                {
+                    sum = sum + i;
+                    i = i - 1;
+                }
+
+            }
+            Console.WriteLine($"Luvun {number} summa = {sum}");
+            
+           
+        }
+    }
+}
+
