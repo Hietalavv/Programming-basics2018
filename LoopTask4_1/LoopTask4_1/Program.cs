a3d08175dbde0f075b9186aae73c86db0630e74a
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-10-16 15:14:45 +0300

    LoopTask4_1 valmis

---
+++ LoopTask4_1/LoopTask4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LoopTask4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo syötetyn luvun (1-10) neliöjuuren");
+            Console.WriteLine("#\tSQRT");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}\t{Math.Sqrt(i)}");
+            }
+        }
+    }
+}
