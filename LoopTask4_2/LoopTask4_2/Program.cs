af24829a7aaa907241d3edfb93570b27cfa3f44f
Ville-Veikko Hietala <v-v.hietala@hotmail.com>
2018-10-17 12:45:17 +0300

    LoopTask4_2 valmis

---
+++ LoopTask4_2/LoopTask4_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopTask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo syötetyn luvun (1-9) kertotaulun");
+            Console.WriteLine("Syötä luku: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"{number}x{i}\t=\t{number*i}"); 
+                
+                
+
+            }
+        }
+    }
+}
